0d6ecae0f9bd64467a657650247d62f9ed9c4e04
xulihang <xulihanghai@163.com>
2023-12-04 16:04:45 +0800

    make license and endpoint public

--- src/main/java/com/tonyxlh/docscan4j/DynamsoftService.java
+++ src/main/java/com/tonyxlh/docscan4j/DynamsoftService.java
@@ -12,8 +12,8 @@ import java.util.Map;
 import okhttp3.*;
 
 public class DynamsoftService {
-    private String endPoint = "http://127.0.0.1:18622";
-    private String license = "";
+    public String endPoint = "http://127.0.0.1:18622";
+    public String license = "";
     private static final MediaType JSON
             = MediaType.get("application/json; charset=utf-8");
     public DynamsoftService(){
